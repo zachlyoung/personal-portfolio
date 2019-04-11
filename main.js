21797f857f9b5ebeb851ba3570045d3d18a6eef3
Zachary Young <zachlyoung2022@gmail.com>
2019-04-11 15:56:42 -0500

    Completed active links

--- main.js
+++ main.js
@@ -5,49 +5,62 @@ var isExpanded;
 
 $(document).ready(function () {
     isAsideOpen = true;
-    if ($(window).width() >= 1000){
+    if ($(window).width() < 1000){
+        isExpanded = false;
+    } else {
         isExpanded = true;
         $('.nav-bar').css('background', '#000');
-        $('#top-bar').toggleClass("exit");
-        $('#middle-bar').toggleClass("exit");
-        $('#bottom-bar').toggleClass("exit");
-        $('#top-bar').toggleClass("open");
-        $('#middle-bar').toggleClass("open");
-        $('#bottom-bar').toggleClass("open");
-    } else {
-        isExpanded = false;
+        toggleNav();
     }
 });
 
 //Resizing
 
 $(window).on('resize', function(){
-    if ($(this).width() < 1000){
-        updateProgressMobile();
-    }
-    if ($(this).width() >= 1000 && !isAsideOpen)
-    {
-        $('aside').show();
-        isAsideOpen = true;
-        isExpanded = true;
-    } else if ($(this).width() < 1000 && isExpanded) {
+    updateProgress();
+    updateActive();
+    if ($(this).width() < 1000 && isExpanded) {
         $('aside').hide();
         isAsideOpen = false;
         isExpanded = false;
+    } else if ($(this).width() >= 1000 && !isAsideOpen) {
+        $('aside').show();
+        isAsideOpen = true;
+        isExpanded = true;
     }
 });
 
 //Progress Bar
 
 $('main').scroll(function(){
-    if ($(window).width() < 1000){
-        updateProgressMobile();
-    }
+    updateProgress();
+    updateActive();
 });
 
-function updateProgressMobile() {
-    var percentage = $('main').scrollTop() / ($('main')[0].scrollHeight - $(window).height()) * 100;
-    $('#progress-bar-mobile').width(percentage + '%');
+function updateProgress() {
+    if ($(window).width() < 1000) {
+        var percentage = $('main').scrollTop() / ($('main')[0].scrollHeight - $(window).height()) * 100;
+        $('#progress-bar-mobile').width(percentage + '%');
+    }
+}
+
+function updateActive() {
+    if ($("main").scrollTop() + $(window).height() > $('main')[0].scrollHeight - 5) {
+        $(".link").removeClass("active");
+        $(".link").eq(4).addClass("active");
+    } else if ($("main").scrollTop() < $("#code").position().top) {
+        $(".link").removeClass("active");
+        $(".link").eq(0).addClass("active");
+    } else if ($("main").scrollTop() < $("#design").position().top) {
+        $(".link").removeClass("active");
+        $(".link").eq(1).addClass("active");
+    } else if ($("main").scrollTop() < $("#music").position().top) {
+        $(".link").removeClass("active");
+        $(".link").eq(2).addClass("active");
+    } else if ($("main").scrollTop() < $("#blog").position().top) {
+        $(".link").removeClass("active");
+        $(".link").eq(3).addClass("active");
+    }
 }
 
 //Navigation
@@ -55,22 +68,12 @@ function updateProgressMobile() {
 $('#button-nav').click(function() {
     if (isAsideOpen) {
         $('.nav-bar').css('background', '#000');
-        $('#top-bar').toggleClass("exit");
-        $('#middle-bar').toggleClass("exit");
-        $('#bottom-bar').toggleClass("exit");
-        $('#top-bar').toggleClass("open");
-        $('#middle-bar').toggleClass("open");
-        $('#bottom-bar').toggleClass("open");
+        toggleNav();
         $('aside').fadeOut();
         isAsideOpen = false;
     } else {
         $('.nav-bar').css('background', '#fff');
-        $('#top-bar').toggleClass("exit");
-        $('#middle-bar').toggleClass("exit");
-        $('#bottom-bar').toggleClass("exit");
-        $('#top-bar').toggleClass("open");
-        $('#middle-bar').toggleClass("open");
-        $('#bottom-bar').toggleClass("open");
+        toggleNav();
         $('aside').fadeIn();
         isAsideOpen = true;
     }
@@ -78,10 +81,19 @@ $('#button-nav').click(function() {
 
 $('.link').click(function () {
     var section = $(this).text().toLowerCase();
-    $('main').scrollTop($('main').scrollTop() + $('#' + section).position().top);
+    $('main').scrollTop($('#' + section).position().top);
     if ($(window).width() < 1000) {
         $('.nav-bar').css('background', '#000');
         $('aside').fadeOut();
         isAsideOpen = false;
     }
 });
+
+function toggleNav() {
+    $('#top-bar').toggleClass("exit");
+    $('#middle-bar').toggleClass("exit");
+    $('#bottom-bar').toggleClass("exit");
+    $('#top-bar').toggleClass("open");
+    $('#middle-bar').toggleClass("open");
+    $('#bottom-bar').toggleClass("open");
+}
