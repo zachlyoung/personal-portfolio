7ab061e3cf150a4f9b69810ab0b0311e9b026f34
Zachary Young <zachlyoung2022@gmail.com>
2019-04-11 02:31:10 -0500

    Completed aside and progress bar animation for mobile devices

--- main.js
+++ main.js
@@ -1,23 +1,64 @@
-$(".link").click(function () {
-    var section = $(this).text().toLowerCase();
-    if ($(window).width() >= 1000) {
-        $("main").scrollTop($("#" + section).position().top);
-    } else {
-        $("html").scrollTop($("#" + section).position().top);
-    }
+//Initialization
+
+var isAsideOpen;
+var isExpanded;
+
+$(document).ready(function () {
+    isAsideOpen = true;
+    isExpanded = false;
 });
+
+//Resizing
 
 $(window).on('resize', function(){
-    var w = $(this);
-    if (w.width() >= 1000) {
+    if ($(this).width() < 1000){
+        updateProgressMobile();
+    }
+    if ($(this).width() >= 1000 && !isAsideOpen)
+    {
         $('aside').show();
+        isAsideOpen = true;
+        isExpanded = true;
+    } else if ($(this).width() < 1000 && isExpanded) {
+        $('aside').hide();
+        isAsideOpen = false;
+        isExpanded = false;
     }
 });
 
-$('#button-open').click(function() {
-    $('aside').show();
-})
+//Progress Bar
 
-$('#button-close').click(function() {
-    $('aside').hide();
-})
+$('main').scroll(function(){
+    if ($(window).width() < 1000){
+        updateProgressMobile();
+    }
+});
+
+function updateProgressMobile() {
+    var percentage = $('main').scrollTop() / ($('main')[0].scrollHeight - $(window).height()) * 100;
+    $('#progress-bar-mobile').width(percentage + '%');
+}
+
+//Navigation
+
+$('#button-nav').click(function() {
+    if (isAsideOpen) {
+        $('.nav-bar').css('background', '#000');
+        $('aside').fadeOut();
+        isAsideOpen = false;
+    } else {
+        $('.nav-bar').css('background', '#fff');
+        $('aside').fadeIn();
+        isAsideOpen = true;
+    }
+});
+
+$('.link').click(function () {
+    var section = $(this).text().toLowerCase();
+    $('main').scrollTop($('main').scrollTop() + $('#' + section).position().top);
+    if ($(window).width() < 1000) {
+        $('.nav-bar').css('background', '#000');
+        $('aside').fadeOut();
+        isAsideOpen = false;
+    }
+});
